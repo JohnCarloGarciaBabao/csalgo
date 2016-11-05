fad3192700566cf506ddd40f5cbc7eef15f04789
JohnCarloGarciaBabao <jgbabao@gmail.com>
2016-11-05 19:53:18 +0800

    lexicographicgenerator

---
+++ lexicographicgenerator/src/lexicographicgenerator/Lexicographicgenerator.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lexicographicgenerator;
+
+/**
+ *
+ * @author jgbabao
+ */
+public class Lexicographicgenerator {
+
+   
+    public static void main(String[] args) {
+        String charset = "ABC";
+        for(int i = 0; i<charset.length(); i++){
+            System.out.println(charset.charAt(i));
+        }
+    }
+    
+}
